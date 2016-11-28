8e51a0fe58b41b4b0ffb63244699f02f5bb5e8ae
Graham Wimbrow <graham.wimbrow@gmail.com>
2016-11-28 15:00:17 -0500

    working, but quickly exhausts V8's mem heap

--- config/handler.js
+++ config/handler.js
@@ -2,14 +2,45 @@ const Promise = require('bluebird');
 const views = require('../pbi');
 const listViews = Object.keys(views);
 
-const query = (base, {table, field}, link) => {
-  return new Promise((resolve, reject) => {
+const query = (base, {field, table, track}, link) => {
+  return track ? compile(base, track) : new Promise((resolve, reject) => {
     base(table).find(link, (err, found) => {
       if (err) {
         reject(err);
       }
       resolve(found.fields[field]);
     });
+  });
+}
+
+const compile = (base, view) => {
+  return new Promise((resolve, reject) => {
+    let response = [];
+    base(views[view].table).select({
+      maxRecords: 1
+    }).eachPage((records, next) => {
+      let promised = [];
+      records.forEach(record => {
+        let formed = {};
+        views[view].keep.forEach(prop => {
+
+          if (prop.alias && record.fields[prop.alias]) {
+
+            formed[prop.alias] = record.fields[prop.alias].length ? [] : null;
+
+            record.fields[prop.alias].forEach(link => {
+              promised.push(query(base, prop, link).then(found => {
+                formed[prop.alias].push(found);
+              }));
+            });
+          } else {
+            formed[prop] = record.fields[prop] || null;
+          }
+        });
+        response.push(formed);
+      });
+      Promise.all(promised).then(() => next()).catch(err => console.log(err));
+    }, err => err ? reject(err) : resolve(response));
   });
 }
 
@@ -28,29 +59,9 @@ module.exports = {
   // returns data for the supplied view (reference ../pbi.json)
   getView: (req, res) => {
     let view = req.params.view;
-    let response = [];
     if (listViews.indexOf(view) === -1) {
       throw new Error(`supplied view must be one of ${listViews.join(', ')}`)
     }
-    req.base(views[view].table).select().eachPage((records, next) => {
-      let promised = [];
-      records.forEach(record => {
-        let formed = {};
-        views[view].keep.forEach(prop => {
-          if (prop.alias) {
-            formed[prop.alias] = record.fields[prop.alias].length ? [] : null;
-            record.fields[prop.alias].forEach(link => {
-              promised.push(query(req.base, prop, link).then((found) => {
-                formed[prop.alias].push(found);
-              }));
-            });
-          } else {
-            formed[prop] = record.fields[prop] || null;
-          }
-        });
-        response.push(formed);
-      });
-      Promise.all(promised).then(() => next()).catch(err => console.log(err));
-    }, err => err ? res.send(err) : res.json(response));
+    compile(req.base, view).then(response => res.json(response)).catch(err => res.send(err));
   }
 }
